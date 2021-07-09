41ad111c4319ee40bf10c731ea30de7d66c6efe3
Cezar <cezarguimaraes@gmail.com>
2021-07-09 01:25:25 -0300

    Exercicios 5.3 concluidos

--- bloco.5/5.3/script.js
+++ bloco.5/5.3/script.js
@@ -73,9 +73,102 @@ function changeHolidayColor(){
 };
 changeHolidayColor(); 
 
+function sextou(diaDaSemana){
+  let getFriday = document.querySelector(".buttons-container");
+  let fridayButton = document.createElement("button");
+  fridayButton.innerHTML = "Sexta!";
+  fridayButton.id = "btn-friday";
+  getFriday.appendChild(fridayButton);
+}
+sextou("Sexta-feira");
 
+function displaySexta(){
+  let getFridayButton = document.querySelector("#btn-friday");
+  let getFridayDays = document.querySelectorAll(".friday");
+  let fridayNewText = "Sextou!";
+  
 
+  getFridayButton.addEventListener("click",function(){
+    for(let index = 0; index < getFridayDays.length; index +=1){
+      if(getFridayDays[index].innerHTML !== fridayNewText){
+        getFridayDays[index].innerHTML = fridayNewText;
+      } else {
+        getFridayDays[index].innerHTML = dezFridays[index]; 
+      }
+    }
+  });
+}
+let dezFridays = [4,11,18,25];
+displaySexta(dezFridays);
 
+function zoomMouseOver(){
+  let getDay = document.querySelector('#days');
+  getDay.addEventListener("mouseover", function(event){
+    event.target.style.fontSize = "30px";
+    event.target.style.fontWeight = "600";
+  })
+}
+zoomMouseOver();
+
+function zoomMouseOut(){
+  let getDay = document.querySelector('#days');
+  getDay.addEventListener("mouseout", function(event){
+    event.target.style.fontSize = "20px";
+    event.target.style.fontWeight = "200";
+  })
+}
+zoomMouseOut();
+
+function AddTask(tarefa){
+  let getDiv = document.querySelector(".my-tasks");
+  let newSpan = document.createElement("span");
+  newSpan.innerHTML = tarefa;
+  getDiv.appendChild(newSpan);
+}
+AddTask("Cozinhar");
+
+function addColor(color){
+  let getDiv = document.querySelector(".my-tasks");
+  let newDiv = document.createElement("div");
+  newDiv.className = "task";
+  newDiv.style.backgroundColor = color;
+  getDiv.appendChild(newDiv);
+}
+addColor("green");
+
+function setTaskClass() {
+  let selectedTask = document.getElementsByClassName('task selected');
+  let myTasks = document.querySelector('.task');
+
+  myTasks.addEventListener('click', function(event) {
+    if (selectedTask.length === 0) {
+      event.target.className = 'task selected';
+    } else {
+      event.target.className = 'task';
+    }
+  });
+};
+
+setTaskClass();
+
+function setDayColor() {
+  let selectedTask = document.getElementsByClassName('task selected');
+  let days = document.querySelector('#days');
+  let taskDiv = document.querySelector('.task');
+  let taskColor = taskDiv.style.backgroundColor;
+  
+  days.addEventListener('click', function(event){
+    let eventTargetColor = event.target.style.color;
+    if (selectedTask.length > 0 && eventTargetColor !== taskColor) {
+      let color = selectedTask[0].style.backgroundColor;
+      event.target.style.color = color;
+    } else if (eventTargetColor === taskColor && selectedTask.length > 0) {
+      event.target.style.color = 'rgb(119,119,119)';
+    }
+  });
+};
+
+setDayColor();
 
 
 
