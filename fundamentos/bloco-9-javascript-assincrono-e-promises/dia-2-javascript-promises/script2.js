25747e9ab4ed5a75de2d5ae3ba3cedf40e6cdb57
Cezar <cezarguimaraes@gmail.com>
2021-08-13 21:36:47 -0300

    dia 2 done

--- fundamentos/bloco-9-javascript-assincrono-e-promises/dia-2-javascript-promises/script2.js
+++ fundamentos/bloco-9-javascript-assincrono-e-promises/dia-2-javascript-promises/script2.js
@@ -7,12 +7,13 @@ const fetchPromise = () => {
       const sum = myArray.map(number => number * number)
                          .reduce((sum, number) => sum + number);
   
-      (sum < 8000) ? resolve() : reject();
+      (sum < 8000) ? resolve(sum) : reject();
     });
   
     myPromise
-      .then(() => console.log('Promise resolvida'))
-      .catch(() => console.log('Promise rejeitada'));
+      .then((sum) => [2, 3, 5, 10].map(number => sum / number))
+      .reduce((acc, curr) => acc + curr,0)
+      .catch(() => console.log('É mais de 8000! Essa promisse deve estar quebrada'));
   };
   
   fetchPromise();
