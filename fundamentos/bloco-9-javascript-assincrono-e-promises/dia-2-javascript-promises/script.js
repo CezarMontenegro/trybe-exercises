9472edeb789204e19482d47b64ada8a678e07377
Cezar <cezarguimaraes@gmail.com>
2021-08-13 21:06:31 -0300

    Exercicio 2

--- fundamentos/bloco-9-javascript-assincrono-e-promises/dia-2-javascript-promises/script.js
+++ fundamentos/bloco-9-javascript-assincrono-e-promises/dia-2-javascript-promises/script.js
@@ -1,11 +1,16 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  fetch(API_URL, { headers: { Accept: 'application/json' } })
+  fetch(API_URL, { headers: { 'Accept': 'application/json' }})
     .then(response => response.json())
-    .then(data =>
-      document.getElementById('jokeContainer').innerText = data.joke
+    .then(data => 
+      document.querySelector('#jokeContainer').innerHTML = data.joke
     );
-}
+    
+};
+
+  
+
 
 window.onload = () => fetchJoke();
+
