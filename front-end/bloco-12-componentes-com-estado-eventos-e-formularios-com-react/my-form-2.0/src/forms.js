a8ca89ce96622da3a7b7a976ddfafd4d8e3bb940
Cezar <cezarguimaraes@gmail.com>
2021-09-05 12:36:38 -0300

    exercicio 1 segundo commit

--- front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/my-form-2.0/src/forms.js
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/my-form-2.0/src/forms.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+const states = ['Rio de Janeiro', 'Minas Gerais', 'Amapá', 'Amazonas', 'São Paulo', 'Ceará', 'Distrito Federal'];
+
 class Forms extends React.Component {
   constructor() {
     super()
@@ -17,7 +19,9 @@ class Forms extends React.Component {
 
   handleChange = (event) => {
     const {name} = event.target;
-    const value = event.target.type === 'checkbox' ? event.target.checkbox : event.target.value;
+    let value = event.target.type === 'checkbox' ? event.target.checkbox : event.target.value;
+
+    if (name === 'nome') value = value.toUpperCase();
     
     this.setState({[name] : value,});
   }
@@ -31,6 +35,8 @@ class Forms extends React.Component {
           type='text'
           value={nome}
           onChange={this.handleChange}
+          maxLength='40'
+          required
         ></input></label>
 
         <label>Email:<input
@@ -38,13 +44,17 @@ class Forms extends React.Component {
           type='emai'
           value={email}
           onChange={this.handleChange}
+          maxLength='50'
+          required
         ></input></label>
 
         <label>CPF:<input
           name='cpf'
-          type='number'
+          type='text'
           value={cpf}
           onChange={this.handleChange}
+          maxLength='11'
+          required
           ></input></label>
 
         <label>Endereço:<input
@@ -52,6 +62,8 @@ class Forms extends React.Component {
           type='text'
           value={endereco}
           onChange={this.handleChange}
+          maxLength='200'
+          required
         ></input></label>
 
         <label>Cidade:<input
@@ -59,24 +71,201 @@ class Forms extends React.Component {
           type='text'
           value={cidade}
           onChange={this.handleChange}
+          maxLength='28'
+          required
         ></input></label>
 
-        <label>Estado:<input
+        <label>Estado:<select
           name='estado'
           type='combobox'
           value={estado}
           onChange={this.handleChange}
-        ></input></label>
+          required
+          >
+          {states.map((value, key) => (
+            <option key={ key }>{ value }</option>))
+          }
+        </select></label>
 
         <label>Tipo:<input
           name='tipo'
           type='radio'
           value={tipo}
           onChange={this.handleChange}
-        ></input></label>
+          required
+        ></input>Casa
+        <input
+          name='tipo'
+          type='radio'
+          value={tipo}
+          onChange={this.handleChange}
+          required
+        ></input>Apartamento</label>
       </fieldset>
     )
   }
 }
 
 export default Forms;
+
+
+
+
+
+// changeHandler = event => {
+//   let { name, value } = event.target;
+
+//   if (name === 'name') value = value.toUpperCase();
+//   if (name === 'address') value = this.validateAddress(value);
+
+//   this.updateState(name, value);
+// }
+
+// onBlurHandler = event => {
+//   let { name, value } = event.target;
+
+//   if (name === 'city') value = value.match(/^\d/) ? '' : value;
+
+//   this.updateState(name, value);
+// }
+
+// updateState(name, value) {
+//   this.setState((state) => ({
+//     [name]: value,
+//     formError: {
+//       ...state.formError,
+//       [name]: this.validateField(name, value)
+//     }
+//   }));
+// }
+
+// validateAddress = address => address.replace(/[^\w\s]/gi, '')
+
+// handleSubmit = event => {
+//   event.preventDefault();
+// }
+
+// validateField(fieldName, value) {
+//   switch (fieldName) {
+//     case 'email':
+//       const isValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2})$/i)
+//       return isValid ? '' : ' is invalid';
+//     default:
+//       break;
+//   }
+//   return '';
+// }
+
+// resetForm = () => { this.setState(INITIAL_STATE) };
+
+// sendForm = () => { this.setState({ submitted: true }) };
+
+
+
+// import React, { Component } from 'react';
+
+// const states = ['Rio de Janeiro', 'Minas Gerais', 'Amapá', 'Amazonas', 'São Paulo', 'Ceará', 'Distrito Federal'];
+
+// class PersonalForm extends Component {
+//   render() {
+//     const { changeHandler, onBlurHandler, currentState } = this.props;
+//     return (
+//       <fieldset>
+//             <legend>Dados pessoais</legend>
+//             <div className="container">
+//               Nome:
+//               <input
+//                 type="name"
+//                 name="name"
+//                 maxLength="40"
+//                 required
+//                 onChange={changeHandler}
+//               />
+//             </div>
+//             <div className="container">
+//               Email:
+//               <input
+//                 type="email"
+//                 name="email"
+//                 maxLength="50"
+//                 required
+//                 onChange={changeHandler}
+//               />
+//             </div>
+//             <div className="container">
+//               CPF:
+//               <input
+//                 type="text"
+//                 name="cpf"
+//                 maxLength="11"
+//                 required
+//                 onChange={changeHandler}
+//               />
+//             </div>
+//             <div className="container">
+//               Endereço:
+//               <input
+//                 type="text"
+//                 name="address"
+//                 maxLength="200"
+//                 required
+//                 onChange={changeHandler}
+//               />
+//             </div>
+//             <div className="container">
+//               Cidade:
+//               <input
+//                 type="text"
+//                 name="city"
+//                 maxLength="28"
+//                 required
+//                 value={currentState.city}
+//                 onBlur={onBlurHandler}
+//                 onChange={changeHandler}
+//               />
+//             </div>
+//             <div className="container">
+//               Estado:
+//               <select
+//                 name="countryState"
+//                 required
+//                 onChange={changeHandler}
+//                 defaultValue=""
+//               >
+//                 <option value="">Selecione</option>
+//                 {
+//                   states.map((value, key) => (
+//                     <option key={ key }>{ value }</option>
+//                   ))
+//                 }
+//               </select>
+//             </div>
+//             <div className="container">
+//               <label htmlFor="house">
+//                 <input
+//                   type="radio"
+//                   id="house"
+//                   name="addressType"
+//                   value="house"
+//                   onChange={changeHandler}
+//                 />
+//                 Casa
+//               </label>
+//               <label htmlFor="apart">
+//                 <input
+//                   type="radio"
+//                   id="apart"
+//                   name="addressType"
+//                   value="apartment"
+//                   onChange={changeHandler}
+//                 />
+//                 Apartamento
+//               </label>
+//             </div>
+//           </fieldset>
+//     );
+//   }
+// }
+
+// export default PersonalForm;
+
