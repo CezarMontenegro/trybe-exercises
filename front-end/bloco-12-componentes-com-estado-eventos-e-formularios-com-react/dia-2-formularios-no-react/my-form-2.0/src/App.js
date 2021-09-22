b78a1b19e948d4d15ff4e726f7b2e986916acfe4
Cezar <cezarguimaraes@gmail.com>
2021-09-22 20:54:06 -0300

    Exercicios dia 2 bloco 12

--- front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/App.js
@@ -1,62 +1,146 @@
-import './App.css';
 import React from 'react';
-import PersonalForm from './PersonalForm';
-import ProfessionalForm from './ProfessionalForm';
-import FormDataDisplay from './FormDataDisplay';
 
-const initialState = {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
       nome: '',
       email: '',
       cpf: '',
-      endereco: '',
-      cidade: '',
+      endereço: '',
       estado: '',
-      tipo: '',
-      curriculo: '',
-      cargo: '',
-      descricao: '',
-      submitted: false,
+      endereçoTipo: '',
+      submit: false,
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange({target}) {
+    const{name, value} = target;
+    this.setState({
+      [name] : value,
+    })
+  }
+
+  handleReset() {
+    this.setState({
+      nome: '',
+      email: '',
+      cpf: '',
+      endereço: '',
+      estado: '',
+      endereçoTipo: '',
+      submit: false,
+    })
+  }
+
+  handleSubmit() {
+    this.setState({ submit: false})
+    if( this.state.submit === false) {
+      this.setState({ submit: true})
+    }
+  }
+
+  render() {
+    const estados = [ "AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO" ];
+    const {nome, email, cpf, endereço, estado, endereçoTipo, submit} = this.state;
+    return (
+      <div>
+        <fieldset>
+          <input
+            name="nome"
+            type="text"
+            maxLength="40"
+            required
+            onChange={this.handleChange}
+            value={ nome }
+          />
+
+          <input  
+            name="email"
+            type="email"
+            maxLength="50"
+            required
+            onChange={this.handleChange}
+            value={ email }
+          />
+
+          <input
+            name="cpf"
+            type="text"
+            maxLength="11"
+            required
+            onChange={this.handleChange}
+            value={ cpf }
+          />
+
+          <input
+            name="endereço"
+            type="text"
+            maxLength="28"
+            required
+            onChange={this.handleChange}
+            value={ endereço }
+          />
+
+          <select
+            name="estado"
+            required
+            onChange={this.handleChange}
+            value={ estado }
+          >
+            {estados.map((estado) => <option value={estado}>{estado}</option>)}
+          </select>
+
+          <label>Casa
+            <input
+              name="endereçoTipo"
+              type="radio"
+              onChange={this.handleChange}
+              value="Casa"
+            />
+          </label>
+
+          <label>Apartamento
+            <input
+              name="endereçoTipo"
+              type="radio"
+              onChange={this.handleChange}
+              value="Apartamento"
+            />
+          </label>
+        </fieldset>
+        {submit && 
+          <div>
+            {`Nome: ${nome}
+            Email: ${email}
+            CPF: ${cpf}
+            Endereço: ${endereço}
+            Estado: ${estado}
+            Tipo: ${endereçoTipo}`}
+          </div>
+        }
+
+        <button
+          type="button"
+          onClick={this.handleSubmit}
+        >
+        Submit
+        </button>
+
+        <button
+          type="button"
+          onClick={this.handleReset}
+        >
+        Reset
+        </button>
+      </div>
+    )
+  }
 }
-
-class App extends React.Component {
-  constructor() {
-    super()
-    
-    this.state = initialState;
-  }
-
-  handleChange = (event) => {
-    const {name} = event.target;
-    let value = event.target.value
-    if(name === 'nome') value = value.toUpperCase();
-    
-    this.setState({[name] : value,});
-  }
-
-  resetForm = () => { this.setState(initialState) };
-
-  sendForm = () => { this.setState({ submitted: true }) };
-
-  render(){
-  return (
-    <div className="App">
-      <PersonalForm valor={this.state} handleChange={this.handleChange}/>
-
-      <ProfessionalForm valor={this.state} handleChange={this.handleChange} />
-
-      <button
-        onClick={this.sendForm}
-        value='Enviar'
-      >Submit</button>
-
-      <button
-        onClick={this.resetForm}
-        value={'Limpar'}
-      >Clear</button>
-
-      { this.state.submitted && <FormDataDisplay valor={this.state} /> }
-    </div>
-  );
-}}
 
 export default App;
