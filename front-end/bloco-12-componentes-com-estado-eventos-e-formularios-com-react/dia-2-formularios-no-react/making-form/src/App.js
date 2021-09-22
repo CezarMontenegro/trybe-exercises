96b439a02df3c25c0948a7b51011e9b764bcd63c
Cezar <cezarguimaraes@gmail.com>
2021-09-22 18:50:38 -0300

    Exercicios dia 2 bloco 12

--- front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/making-form/src/App.js
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/making-form/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Input1 from './Input1';
+import Input2 from './Input2';
 
 class App extends React.Component {
   constructor() {
@@ -9,67 +11,68 @@ class App extends React.Component {
       input2Value: 0,
       textareaValue: '',
       selectValue: '',
+      checkboxValue: '',
+      errorForm: false,
     }
 
     this.handleChangeInput1 = this.handleChangeInput1.bind(this);
-    this.handleChangeInput2 = this.handleChangeInput2.bind(this);
-    this.handleChangeTextarea = this.handleChangeTextarea.bind(this);
-    this.handleChangeSelect = this.handleChangeSelect.bind(this);
+  
+  }
+
+  handleError() {
+    const { input1Value, input2Value } = this.state;
+
+    const errorCases = [
+      !input1Value,
+      !input2Value,
+    ];
+
+    const formularioPreenchido = errorCases.every((error) => error !== true);
+
+    this.setState({
+      formularioComErros: !formularioPreenchido,
+    });
   }
 
   handleChangeInput1({target}) {
+    const {name} = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
-      input1Value : target.value,
-    })
-  }
-
-  handleChangeInput2({target}) {
-    this.setState({
-      input2Value: target.value,
-    })
-  }
-
-  handleChangeTextarea({target}) {
-    this.setState({
-      textareaValue: target.value,
-    })
-  }
-
-  handleChangeSelect({target}) {
-    this.setState({
-      selectValue: target.value,
+      [name] : value,
     })
   }
 
   render() {
     return (
-      <div>
+      <fieldset>
+        <Input1 value={this.state.input1Value} handleChange={this.handleChangeInput1} />
+
+        <Input2 value={this.state.input2Value} handleChange={this.handleChangeInput1} />
+
         <select
-          onChange={this.handleChangeSelect}
+          name="selectValue"
+          onChange={this.handleChangeInput1}
           value={this.state.selectValue}
         >
         <option value="Apple">Apple</option>
         <option value="Banana">Banana</option>
         <option value="Cranberry">Cranberry</option>
         </select>
-        
-        <input
+    
+        <textarea
+          name="textareaValue"
           type="text"
           onChange={this.handleChangeInput1}
-          value={this.state.input1Value}
+          value={this.state.textareaValue}
         />
 
         <input
-          type="number"
-          onChange={this.handleChangeInput2}
-          value={this.state.input2Value}
-        />
-        <textarea 
-          type="text"
-          onChange={this.handleChangeTextarea}
+          name="checkboxValue"
+          type="checkbox"
+          onChange={this.handleChangeInput1}
           value={this.state.textareaValue}
         />
-      </div>
+      </fieldset>
     )
   }
 }
