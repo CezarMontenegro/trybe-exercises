2e0167b7f04619ee207d8ba8ac4bb203fc0274f2
Cezar <cezarguimaraes@gmail.com>
2022-01-19 23:30:36 -0300

    middlewares isUserValid e createUser criados

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/pratica/models/User.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/pratica/models/User.js
@@ -1,1 +1,20 @@
-const 
+const connection = require('./connection');
+
+const isUserValid = (firstName, lastName, email, password) => {
+  if ([firstName, lastName, email, password].some(i => i === !i)) return false;
+  if (password < 3) return false;
+
+  return true;
+}
+
+const createUser = (firstName, lastName, email, password) => {
+  connection.execute(
+    'INSERTO INTO model_example.users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)',
+    [firstName, lastName, email, password]
+  )
+}
+
+module.exports = {
+  isUserValid,
+  createUser,
+}
