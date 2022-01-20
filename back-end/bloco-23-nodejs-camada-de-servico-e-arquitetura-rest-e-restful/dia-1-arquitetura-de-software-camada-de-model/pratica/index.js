175b2094290c67543eaa4c4f77e86db5b83c5b8d
Cezar <cezarguimaraes@gmail.com>
2022-01-19 23:35:07 -0300

    Rota post /users criada

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/pratica/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/pratica/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { isUserValid, createUser } = require('./models/')
+const { isUserValid, createUser } = require('./models/User');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,7 +9,16 @@ app.use(bodyParser.json());
 app.post('/user', async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
+  if(!isUserValid(firstName, lastName, email, password)) {
+    return res.status(400).json({
+      "error": true,
+      "message": "O campo 'password' deve ter pelo menos 6 caracteres"
+    });
+  }
 
+  await createUser(firstName, lastName, email, password); 
+
+  res.status(201).json({ message: 'Autor criado com sucesso'})
 })
 
 app.listen(3002, () => console.log('Escutando na porta 3002'));
